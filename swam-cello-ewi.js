62126319ce13e01d64410ade1c965239a7eba307
Marco Zagha <46510113+marcozarco@users.noreply.github.com>
2019-03-03 16:12:13 -0800

    get out of tabbing hell from logic editor

--- swam-cello-ewi.js
+++ swam-cello-ewi.js
@@ -8,8 +8,8 @@
 
 var PluginParameters =
 [
-		{name:"Sample parameter", type:"linear",
-		minValue:0, maxValue:127, defaultValue:20},
+{name:"Sample parameter", type:"linear",
+minValue:0, maxValue:127, defaultValue:20},
 ];
 
 // Breath 
@@ -81,38 +81,38 @@ var needToRestoreHandPosition;
 var keepHandAtBridge;
 
 function Reset() {
-  Trace("Reset()");
-  lastBreathValue = 0;
-  lastHarmonic = 0;
-  lastPitch = 50;
-  lastVibratoPedal = 0;
-  portamento = false;
-  flautando = false;
-  harmonic = false;
-  notesOn = 0;
-  needToRestoreHandPosition = false;
-  keepHandAtBridge = false;
+    Trace("Reset()");
+    lastBreathValue = 0;
+    lastHarmonic = 0;
+    lastPitch = 50;
+    lastVibratoPedal = 0;
+    portamento = false;
+    flautando = false;
+    harmonic = false;
+    notesOn = 0;
+    needToRestoreHandPosition = false;
+    keepHandAtBridge = false;
 }
 
 function ParameterChanged(param, value) {
-  Trace('parameter ' + param + ' changed to ' + value);
-  Reset(); // fix?
+    Trace('parameter ' + param + ' changed to ' + value);
+    Reset(); // fix?
 }
 
 function sendEvent(event) {
-  event.send();
-  // event.trace();
+    event.send();
+    // event.trace();
 }
 
 function sendCC(number, value) {
-  var cc = new ControlChange;
-  cc.number = number;
-  cc.value = value;
-  sendEvent(cc);
+    var cc = new ControlChange;
+    cc.number = number;
+    cc.value = value;
+    sendEvent(cc);
 } 
 
 function scaleCC(value, minOut, maxOut) {
-  return MIDI.normalizeData(value / 127 * (maxOut - minOut) + minOut);
+    return MIDI.normalizeData(value / 127 * (maxOut - minOut) + minOut);
 }
 
 
@@ -125,30 +125,30 @@ function scaleCC(value, minOut, maxOut) {
 function sendVibrato() {
     var vibratoRate, vibratoDepth;
     if (lastVibratoPedal < minPedalForVibrato || harmonic) {
-      vibratoRate = 0;
-      vibratoDepth = 0;
+        vibratoRate = 0;
+        vibratoDepth = 0;
     } else if (lastVibratoPedal > minPedalForFastVibrato) {
-      vibratoRate = fastVibratoRate;
-      vibratoDepth = fastVibratoDepth;
+        vibratoRate = fastVibratoRate;
+        vibratoDepth = fastVibratoDepth;
     } else {
-      var breathFrac = Math.max(0.0, lastBreathValue - vibratoSmallBreathValue) / 127;
-      var pedalFrac = (lastVibratoPedal - minPedalForVibrato) / (minPedalForFastVibrato - minPedalForVibrato);
-      var noteFrac = Math.max(0, (lastPitch - vibratoMinNoteRange)) / (vibratoMaxNoteRange - vibratoMinNoteRange);
- 
-      // Allow experimenting with weights of pedal, breath, and note using weight parameters.
-      var totalDepthWt = vibratoPedalContrib + vibratoBreathContrib;
-      var breathDepthWt = vibratoBreathContrib / totalDepthWt;
-      var pedalDepthWt = vibratoPedalContrib / totalDepthWt;
-      var totalRateWt = vibratoPedalContrib + vibratoBreathContrib + vibratoNoteContrib;
-      var breathRateWt = vibratoBreathContrib / totalRateWt;
-      var pedalRateWt = vibratoPedalContrib / totalRateWt;
-      var noteRateWt = vibratoNoteContrib / totalRateWt;
-      
-      depthFrac = Math.min(1.0, breathDepthWt * breathFrac + pedalDepthWt * pedalFrac);
-      rateFrac = Math.min(1.0, breathRateWt * breathFrac + pedalRateWt * pedalFrac + noteRateWt * noteFrac);
+        var breathFrac = Math.max(0.0, lastBreathValue - vibratoSmallBreathValue) / 127;
+        var pedalFrac = (lastVibratoPedal - minPedalForVibrato) / (minPedalForFastVibrato - minPedalForVibrato);
+        var noteFrac = Math.max(0, (lastPitch - vibratoMinNoteRange)) / (vibratoMaxNoteRange - vibratoMinNoteRange);
 
-      vibratoDepth = slowVibratoDepth + depthFrac * (maxVibratoDepth - slowVibratoDepth);
-      vibratoRate = slowVibratoRate + rateFrac * (maxVibratoRate - slowVibratoRate);
+        // Allow experimenting with weights of pedal, breath, and note using weight parameters.
+        var totalDepthWt = vibratoPedalContrib + vibratoBreathContrib;
+        var breathDepthWt = vibratoBreathContrib / totalDepthWt;
+        var pedalDepthWt = vibratoPedalContrib / totalDepthWt;
+        var totalRateWt = vibratoPedalContrib + vibratoBreathContrib + vibratoNoteContrib;
+        var breathRateWt = vibratoBreathContrib / totalRateWt;
+        var pedalRateWt = vibratoPedalContrib / totalRateWt;
+        var noteRateWt = vibratoNoteContrib / totalRateWt;
+
+        depthFrac = Math.min(1.0, breathDepthWt * breathFrac + pedalDepthWt * pedalFrac);
+        rateFrac = Math.min(1.0, breathRateWt * breathFrac + pedalRateWt * pedalFrac + noteRateWt * noteFrac);
+
+        vibratoDepth = slowVibratoDepth + depthFrac * (maxVibratoDepth - slowVibratoDepth);
+        vibratoRate = slowVibratoRate + rateFrac * (maxVibratoRate - slowVibratoRate);
     }
     sendCC(vibratoRateCC, MIDI.normalizeData(vibratoRate));
     sendCC(vibratoDepthCC, MIDI.normalizeData(vibratoDepth));
@@ -164,137 +164,137 @@ function sendVibrato() {
 // left thumb (which is supposed to be used for portamento but is a bit hard for me to
 // control), but I'd probably combine it with the pitch delta scaling below.
 function sendPortamentoAfterMovingHandToBridgeIfNeeded(pitch) {
-  var portamentoTime = 0;
-  var pitchDelta = Math.max(1,Math.abs(lastPitch - pitch));
-  if (portamento) {
-    if (pitchDelta >= 8 && pitch > lastPitch) {
-      if (notesOn > 1 || pitch > 65 /* 72 */) { // example: bar 8, octave jmp
-         moveHandToBridge();
-      }
-      if (pitch > 72) { // example: bar 59, big jump to harmonic
-    	    portamentoTime = 127 - maxPortamentoTime;
-      } else { // examples: bar 8 & 25, octave jmp up; bar 20, up 7th; bar 49, up 6th
-      	  portamentoTime = 127 - bigJumpPortamentoTime;
-      }
-    } else if (needToRestoreHandPosition){
-      // examples: bar 24, down 6th; bar 43, up 4th; bar 54 down 3rds; bar 60 up whole step
-      portamentoTime = 127 - maxPortamentoTime;
-    	} else { // examples: bar 14, down tritone; bar 16 up 4th; bar 59 down semitone
-    	  if (pitch < lastPitch - 4) {
-    	    portamentoTime = 127 - bigJumpPortamentoTime;
-    	  } else {
-        portamentoTime = 127 - Math.min(minPortamentoTime * pitchDelta, maxPortamentoTime);
-      }
+    var portamentoTime = 0;
+    var pitchDelta = Math.max(1,Math.abs(lastPitch - pitch));
+    if (portamento) {
+        if (pitchDelta >= 8 && pitch > lastPitch) {
+            if (notesOn > 1 || pitch > 65 /* 72 */) { // example: bar 8, octave jmp
+                moveHandToBridge();
+            }
+            if (pitch > 72) { // example: bar 59, big jump to harmonic
+                portamentoTime = 127 - maxPortamentoTime;
+            } else { // examples: bar 8 & 25, octave jmp up; bar 20, up 7th; bar 49, up 6th
+                portamentoTime = 127 - bigJumpPortamentoTime;
+            }
+        } else if (needToRestoreHandPosition){
+            // examples: bar 24, down 6th; bar 43, up 4th; bar 54 down 3rds; bar 60 up whole step
+            portamentoTime = 127 - maxPortamentoTime;
+        } else { // examples: bar 14, down tritone; bar 16 up 4th; bar 59 down semitone
+            if (pitch < lastPitch - 4) {
+                portamentoTime = 127 - bigJumpPortamentoTime;
+            } else {
+                portamentoTime = 127 - Math.min(minPortamentoTime * pitchDelta, maxPortamentoTime);
+            }
+        }
     }
-  }
-  sendCC(portamentoCC, MIDI.normalizeData(portamentoTime));
+    sendCC(portamentoCC, MIDI.normalizeData(portamentoTime));
 }
 
 var HandPosition = {
-  MID: 0,
-  BRIDGE: 63,
-  NUT: 127,
+MID: 0,
+     BRIDGE: 63,
+     NUT: 127,
 };
 
 function restoreHandPosition() {
-  if (keepHandAtBridge) {
-    // Wait for one more NoteOff before restoring hand position.
-    // TODO(marcoz): find a better way to determine when to move hand back.
-    keepHandAtBridge = false;
-    return;
-  }
-  if (needToRestoreHandPosition) {
-    needToRestoreHandPosition = false;
-    sendCC(altFingeringCC, HandPosition.MID);
-  }
+    if (keepHandAtBridge) {
+        // Wait for one more NoteOff before restoring hand position.
+        // TODO(marcoz): find a better way to determine when to move hand back.
+        keepHandAtBridge = false;
+        return;
+    }
+    if (needToRestoreHandPosition) {
+        needToRestoreHandPosition = false;
+        sendCC(altFingeringCC, HandPosition.MID);
+    }
 }
 
 function moveHandUpTowardsNut() {
-  if (needToRestoreHandPosition) {
-   // We've already moved hand, so we'll ignore the single-string-portamento CC.
-    return;
-  }
-  needToRestoreHandPosition = true;
-  sendCC(altFingeringCC, HandPosition.NUT);
+    if (needToRestoreHandPosition) {
+        // We've already moved hand, so we'll ignore the single-string-portamento CC.
+        return;
+    }
+    needToRestoreHandPosition = true;
+    sendCC(altFingeringCC, HandPosition.NUT);
 }
 
 function moveHandToBridge() {
-  keepHandAtBridge = true;
-  needToRestoreHandPosition = true;
-  sendCC(altFingeringCC, HandPosition.BRIDGE);
+    keepHandAtBridge = true;
+    needToRestoreHandPosition = true;
+    sendCC(altFingeringCC, HandPosition.BRIDGE);
 }
 
 function sendBowPressureAndPosition() { // based on breath, flautando, and harmonic
-  var bowPressure, bowPosition;
-  if (flautando) {
-    bowPressure = scaleCC(lastBreathValue, minFlautandoBowPressure, maxFlautandoBowPressure);
-    bowPosition = flautandoBowPosition;
-  } else {
-    bowPressure = scaleCC(lastBreathValue, minBowPressure, maxBowPressure);
-    if (harmonic) {
-      bowPosition = harmonicBowPosition;
+    var bowPressure, bowPosition;
+    if (flautando) {
+        bowPressure = scaleCC(lastBreathValue, minFlautandoBowPressure, maxFlautandoBowPressure);
+        bowPosition = flautandoBowPosition;
     } else {
-      bowPosition = scaleCC(lastBreathValue, minBowPosition, maxBowPosition);
+        bowPressure = scaleCC(lastBreathValue, minBowPressure, maxBowPressure);
+        if (harmonic) {
+            bowPosition = harmonicBowPosition;
+        } else {
+            bowPosition = scaleCC(lastBreathValue, minBowPosition, maxBowPosition);
+        }
     }
-  }
-  sendCC(bowPressureCC, bowPressure);
-  sendCC(bowPositionCC, bowPosition);
+    sendCC(bowPressureCC, bowPressure);
+    sendCC(bowPositionCC, bowPosition);
 }
 
 
 function HandleMIDI(event) {
-  if (event instanceof ControlChange) {
-    switch(event.number) {
-      case breathCC:
-        lastBreathValue = event.value;
+    if (event instanceof ControlChange) {
+        switch(event.number) {
+            case breathCC:
+                lastBreathValue = event.value;
+                sendEvent(event);
+                sendBowPressureAndPosition();
+                sendVibrato();
+                break;
+            case vibratoPedalCC:
+                lastVibratoPedal = event.value;
+                sendVibrato();  // swallow pedal event
+                break;
+            case flautandoCC:
+                flautando = (event.value > 0);
+                sendBowPressureAndPosition(); // swallow event
+                break;
+            case singleStringPortamentoCC:
+                if (event.value >= 32) moveHandUpTowardsNut();
+                // fall through [commenting per Section 5.8.3.1 of the javascript style guide]
+            case portamentoCC:
+                // Save state for next NoteOn.
+                portamento = (event.value >= 32); // just enough to avoid accidental touch
+                break;
+            case harmonicCC:
+                harmonic = event.value; // swallow event;
+                break;
+        }
+    } else if (event instanceof NoteOff && event.pitch <= 36) {
         sendEvent(event);
-        sendBowPressureAndPosition();
-        sendVibrato();
-        break;
-      case vibratoPedalCC:
-        lastVibratoPedal = event.value;
-        sendVibrato();  // swallow pedal event
-        break;
-      case flautandoCC:
-        flautando = (event.value > 0);
-      	  sendBowPressureAndPosition(); // swallow event
-     	  break;
-      case singleStringPortamentoCC:
-        if (event.value >= 32) moveHandUpTowardsNut();
-        // fall through [commenting per Section 5.8.3.1 of the javascript style guide]
-      case portamentoCC:
-        // Save state for next NoteOn.
-    	    portamento = (event.value >= 32); // just enough to avoid accidental touch
-    	    break;
-      case harmonicCC:
-        harmonic = event.value; // swallow event;
-        break;
-    }
-  } else if (event instanceof NoteOff && event.pitch <= 36) {
-    sendEvent(event);
-    Trace('All off ' + event.pitch);
-    MIDI.allNotesOff();
-    Reset();
-  } else if (event instanceof NoteOn) {
-    notesOn++;
-    if (harmonic > 0) {
-      event.velocity = 1; // Kill velocity with harmonics.
-      sendCC(bowPositionCC, harmonicBowPosition);
+        Trace('All off ' + event.pitch);
+        MIDI.allNotesOff();
+        Reset();
+    } else if (event instanceof NoteOn) {
+        notesOn++;
+        if (harmonic > 0) {
+            event.velocity = 1; // Kill velocity with harmonics.
+            sendCC(bowPositionCC, harmonicBowPosition);
+        } else {
+            event.velocity = scaleCC(event.velocity, minVelocity, maxVelocity);
+        }
+        if (harmonic != lastHarmonic) {
+            sendCC(harmonicCC, harmonic);
+            lastHarmonic = harmonic;
+        }
+        sendPortamentoAfterMovingHandToBridgeIfNeeded(event.pitch);
+        lastPitch = event.pitch;
+        sendEvent(event);
+    } else if (event instanceof NoteOff) {
+        notesOn--;
+        restoreHandPosition();
+        sendEvent(event);
     } else {
-      event.velocity = scaleCC(event.velocity, minVelocity, maxVelocity);
+        sendEvent(event);
     }
-    if (harmonic != lastHarmonic) {
-      sendCC(harmonicCC, harmonic);
-      lastHarmonic = harmonic;
-    }
-    sendPortamentoAfterMovingHandToBridgeIfNeeded(event.pitch);
-	  lastPitch = event.pitch;
-    sendEvent(event);
-  } else if (event instanceof NoteOff) {
-    notesOn--;
-    restoreHandPosition();
-    sendEvent(event);
-  } else {
-    sendEvent(event);
-  }
 }
